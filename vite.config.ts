ed339830c2c438049ea71b08692239e921fae788
Sarisia <git@sarisia.cc>
2026-06-07 16:36:02 +0000

    deploy let's go

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/vspo-stream-schedule/",
+  base: "/",
   server: { host: "0.0.0.0" },
   build: { outDir: "build" },
 });
